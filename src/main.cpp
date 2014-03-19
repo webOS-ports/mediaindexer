a6c143bde0b4fbc799c5ea25b32abde26dbeffeb
Simon Busch <morphis@gravedo.de>
2014-03-19 13:12:21 +0100

    Remove qt5 dependency and old code
    Signed-off-by: Simon Busch <morphis@gravedo.de>

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
-#include "mediaindexer.h"
-
 int main(int argc, char **argv)
 {
-    MediaIndexer mi(argc, argv);
-    return mi.exec();
+    return 0;
 }
